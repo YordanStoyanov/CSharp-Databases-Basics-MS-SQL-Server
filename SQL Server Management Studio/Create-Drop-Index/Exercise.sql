85add104122323d22136558e54a1b34df4bfc764
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-05-31 15:09:40 +0300

    Add files via upload

---
+++ SQL Server Management Studio/Create-Drop-Index/Exercise.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(*) FROM Employees
+
+SELECT * FROM Employees
+
+CREATE INDEX X_Employees 
+ON Employees(EmployeeID)
+
+CREATE INDEX XX_Employees
+ON Employees(AddressID)
+
+SELECT EmployeeID FROM Employees
+
+DROP INDEX X_Employees 
+ON Employees
